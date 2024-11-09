691c9124e83912b293c5e9845b64c969ecb2e6a5
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-11-10 00:17:06 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import * as net from "net";
 import { RESP } from "./parser";
 
+let map: Map<string, string> = new Map<string, string>();
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 const server: net.Server = net.createServer((connection: net.Socket) => {
 	connection.on("data", (data) => {
@@ -15,6 +16,17 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
 			connection.write(`$${message.length}\r\n${message}\r\n`);
 		}
 		if (command.toLowerCase() === "set") {
+			const key = command_sequence[1] as string;
+			const value = command_sequence[2] as string;
+			map.set(key, value);
+			connection.write("+OK\r\n");
+		}
+		if (command.toLowerCase() === "get") {
+			const key = command_sequence[1] as string;
+			const value = map.get(key);
+			if (value !== undefined) {
+				connection.write(`$${value.length}\r\n${value}\r\n`);
+			}
 		}
 	});
 });
