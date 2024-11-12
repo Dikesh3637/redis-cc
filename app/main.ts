0de8f2a8265ed36064185fd7c1ec379f04e4d7b3
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-11-12 22:46:40 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,6 +8,7 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
 	connection.on("data", (data) => {
 		const parser = new RESP(data.toString());
 		const command_sequence = parser.decode();
+		console.log(command_sequence);
 		let command = command_sequence[0] as string;
 		if (command.toLowerCase() === "ping") {
 			connection.write("+PONG\r\n");
